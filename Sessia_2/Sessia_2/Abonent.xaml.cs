dec8a76d6b60b2f73c724f6dc2d158f2d60ac1f3
ainyrbikmurzin <96476318+ainyrbikmurzin@users.noreply.github.com>
2022-05-16 15:37:08 +0300

    Add files via upload

---
+++ Sessia_2/Sessia_2/Abonent.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Sessia_2.Classes;
+
+namespace Sessia_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Abonent.xaml
+    /// </summary>
+    public partial class Abonent : Page
+    {
+        public Abonent()
+        {
+            InitializeComponent();
+            AbonentGrid.ItemsSource = Entities.GetContext().Table_1.ToList();
+        }
+    }
+}
---
+++ Sessia_2/Sessia_2/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Sessia_2.Classes;
+
+namespace Sessia_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Abonent());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void Prof_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Question_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void support_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void support_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void support_Copy2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void support_Copy5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void support_Copy4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
