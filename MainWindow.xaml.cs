faa34966af3781479a19673b4d0005965ed6725a
ainyrbikmurzin <96476318+ainyrbikmurzin@users.noreply.github.com>
2022-05-23 11:38:42 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sessia_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Update_Click_1(object sender, RoutedEventArgs e)
+        {
+            {
+                Method P = new Method();
+                var code = P.GetCode();
+                MessageBox.Show(code, "Генератор кода");
+            }
+        }
+
+        private void Password_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TextBox txtBox = e.Source as TextBox;
+                if (txtBox != null)
+                {
+                    Method P = new Method();
+                    var code = P.GetCode();
+                    MessageBox.Show(code, "Генератор кода");
+                    
+                }
+            }
+        }
+
+        private void Code_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            NumbeR.Clear();
+            Password.Clear();
+            Code.Clear();
+            MessageBox.Show("Поля очищены", "Отмена");
+        }
+
+        private void Entrance_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumbeR_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TextBox txtBox = e.Source as TextBox;
+                if (txtBox != null)
+                {
+                    Password.IsEnabled = IsEnabled;
+                    Code.IsEnabled = IsEnabled;
+                    Entrance.IsEnabled = IsEnabled;
+                }
+            }
+        }
+        class Method
+        {
+            public string GetCode()
+            {
+                int[] arr = new int[8];
+                Random rnd = new Random();
+                string Code = "";
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(33, 125);
+                    Code += (char)arr[i];
+                }
+                return Code;
+            }
+        }
+    }
+}
